8f65d91140bde589bab2f3bf57b0a68dcbe141a9
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-02-17 21:31:50 -0600

    supress new relic log file

--- server.js
+++ server.js
@@ -3,18 +3,20 @@
 var express = require('express');
 require('newrelic'); // Heroku performance logging
 
-var server = express();
+var app = express();
 
-server.configure(function(){
-  server.use('/', express.static(__dirname + '/app'));
+app.configure(function(){
+  app.use('/', express.static(__dirname + '/app'));
 });
 
-server.get('/about.txt', function(req, res){
+app.get('/about.txt', function(req, res){
   var body = 'sURI, hands on access to the world\'s data!';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', Buffer.byteLength(body));
   res.end(body);
 });
 
-server.listen(8002);
-console.log('sURI started on port 8002');
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
