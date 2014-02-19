38bc7f686f4c76b0d6b4e0a6305f0bfbdd8b959e
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-02-18 21:14:20 -0600

    proxy bug

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var express   = require('express'),
 
 var apiProxy = function apiProxy(req, res, next) {
   var apiHost = req.get('api-host');
-  if(apiHost && apiHost !== 'suri.io') {
-    proxy.web(req, res, {target: 'http://' + apiHost});
+  if(apiHost) {
+    proxy.web(req, res, {target: apiHost});
   } else {
     next();
   }
