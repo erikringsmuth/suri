7477784196ade8eb0b44b960497dc88ff684df16
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-02-18 20:42:43 -0600

    working on proxy

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 'use strict';
 
-var express = require('express');
-require('newrelic'); // Heroku performance logging
+// Heroku performance logging
+require('newrelic');
 
-var app = express();
+var express   = require('express'),
+    proxy     = require('http-proxy').createProxyServer(),
+    app       = express();
 
-app.configure(function(){
-  app.use('/', express.static(__dirname + '/app'));
+var apiProxy = function apiProxy(req, res, next) {
+  var apiHost = req.get('api-host');
+  if(apiHost && apiHost !== 'suri.io') {
+    proxy.web(req, res, {target: apiHost});
+  } else {
+    next();
+  }
+};
+
+proxy.on('error', function (err, req, res) {
+  res.writeHead(res.statusCode, res._headers);
+  res.end('Connection failed.');
 });
 
+app.configure(function(){
+  app.use(apiProxy); // Proxy routing
+  app.use(express.static(__dirname + '/app')); // Static content
+});
+
+// Welcome text
 app.get('/about.txt', function(req, res){
   var body = 'sURI, hands on access to the world\'s data!';
   res.setHeader('Content-Type', 'text/plain');
@@ -16,6 +34,7 @@ app.get('/about.txt', function(req, res){
   res.end(body);
 });
 
+// Start server
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log('Listening on ' + port);
