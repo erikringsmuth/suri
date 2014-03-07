54b1a2bbc3f1899ed981151c11cf4b5fab37b48f
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-03-07 09:58:53 -0600

    xhr schema

--- shell_scripts/populateElasticsearch.js
+++ shell_scripts/populateElasticsearch.js
@@ -1,29 +1,91 @@
 // Copyright (C) 2014 Erik Ringsmuth <erik.ringsmuth@gmail.com>
 'use strict';
 
+var Xhr = function(xhr) {
+  this.name = xhr.name || 'XHR';
+  this.method = xhr.method || 'GET';
+  this.url = xhr.url || 'http://';
+  this.info = xhr.info || null;
+  this.createdDate = xhr.createdDate || Date.now();
+  this.changedDate = xhr.changedDate || Date.now();
+  this.callCount = xhr.callCount || 0;
+
+  // headers: [
+  //   {
+  //     header: 'Content-Type',
+  //     values: ['application/json', 'application/xml'],
+  //     default: 'application/json',
+  //     required: false
+  //   }
+  // ],
+  this.headers = xhr.headers || [
+    {
+      header: 'Content-Type',
+      values: ['application/json', 'application/xml'],
+      default: 'application/json',
+      required: false
+    }
+  ];
+  this.body = xhr.body || '';
+  this.corsEnabled = xhr.corsEnabled || false;
+  this.depricated = xhr.depricated || false;
+  this.isPublic = xhr.isPublic || true;
+  this.tags = xhr.tags || [];
+
+  // stars: [
+  //   {
+  //     user: '',
+  //     date: Date.now()
+  //   }
+  // ]
+  this.stars = xhr.stars || [];
+
+  // user GUID
+  this.owner = xhr.owner || null;
+
+
+  // forks: [
+  //   'forkedId'
+  // ],
+  this.forks = xhr.forks || [];
+  this.forkedFrom = xhr.forkedFrom || null;
+};
+
+
+//// Data
+var xhrs = [
+  new Xhr({
+    name: 'What\'s my IP?',
+    method: 'GET',
+    url: 'http://www.suri.io/ip',
+    info: 'Get your local machine\'s IP address'
+  }),
+  new Xhr({
+    name: 'Google Search',
+    method: 'GET',
+    url: 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={{searchTerm}}',
+    info: null
+  }),
+  new Xhr({
+    name: 'Google Typeahead',
+    method: 'GET',
+    url: 'http://suggestqueries.google.com/complete/search?client=firefox&q={{searchTerm}}',
+    info: null
+  })
+];
+
+
+//// Script
 var elasticsearch = require('elasticsearch'),
-    shortId = require('shortid');
+    shortId = require('shortid'),
+    index = 'suri-ci',
+    type = 'xhr';
 
 var client = elasticsearch.Client({
   host: 'localhost:9200'
 });
-var index = 'suri-ci',
-    type = 'xhr';
 
-var addXhr = function(xhr) {
-  client.index({
-    index: index,
-    type: type,
-    id: shortId.generate(),
-    body: xhr
-  })
-    .then(function (body) {
-      console.log('\nCreated: ' + JSON.stringify(body));
-    }, function (error) {
-      console.log(error);
-    });
-};
-
+// Delete all existing items in the index
 client.deleteByQuery({
   index: index,
   type: type,
@@ -33,31 +95,25 @@ client.deleteByQuery({
     }
   }
 })
-  .then(function (body) {
+  .then(function () {
     console.log('Deleted existing items');
 
-    addXhr({
-      name: 'What\'s my IP?',
-      url: 'http://www.suri.io/ip',
-      method: 'GET',
-      info: 'Get your local machine\'s IP address'
+    // Then add each of the new items
+    xhrs.forEach(function (xhr) {
+      client.index({
+        index: index,
+        type: type,
+        id: shortId.generate(),
+        body: xhr
+      })
+        .then(function (body) {
+          console.log('\nCreated: ' + JSON.stringify(body));
+        }, function (error) {
+          console.log(error);
+        });
     });
 
-    addXhr({
-      name: 'Google Search',
-      url: 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={{searchTerm}}',
-      method: 'GET',
-      info: ''
-    });
-
-    addXhr({
-      name: 'Google Typeahead',
-      url: 'http://suggestqueries.google.com/complete/search?client=firefox&q={{searchTerm}}',
-      method: 'GET',
-      info: ''
-    });
-
-  }, function (error) {
+  }, function () {
     console.log('Failed to delete existing items.');
   });
 
