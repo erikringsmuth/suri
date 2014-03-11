12c44bc1fc04f383b54ce449e177bcd11800b3db
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-03-10 20:32:37 -0500

    faster auth

--- server/authentication/googleOAuth2.js
+++ server/authentication/googleOAuth2.js
@@ -4,7 +4,7 @@
 var https = require('https'),
     jwt = require('jwt-simple'),
     jws = require('jws'),
-    googleCertificates = [];
+    googleCertificates = {};
 
 
 
@@ -30,13 +30,7 @@ var getGoogleCertificates = function getGoogleCertificates(callback) {
       //  "2784873a666fc894d6c724df0c26c6296493e758": "-----BEGIN CERTIFICATE-----\nMIICITCCAYqgAwIBAgIIN5YrSIb+nWIwDQYJKoZIhvcNAQEFBQAwNjE0MDIGA1UE\nAxMrZmVkZXJhdGVkLXNpZ25vbi5zeXN0ZW0uZ3NlcnZpY2VhY2NvdW50LmNvbTAe\nFw0xNDAzMDkwNjI4MzRaFw0xNDAzMTAxOTI4MzRaMDYxNDAyBgNVBAMTK2ZlZGVy\nYXRlZC1zaWdub24uc3lzdGVtLmdzZXJ2aWNlYWNjb3VudC5jb20wgZ8wDQYJKoZI\nhvcNAQEBBQADgY0AMIGJAoGBALB5HLJsOF9hdSUU0B2uiyWsUmKCuqQJaExp7DML\npjgDJV92+iVdHcu1mHtuB2LnzgwlOkOJp9g7YeUoU+HhX2bFVXVz5R5z1vyeApKq\n6/MnRym2XcZsi0IqEDPU8IA3ZdL2P4a/qRpAgTkVLGOXLVAe/FTpDKQ68acvj5Yq\nXKCNAgMBAAGjODA2MAwGA1UdEwEB/wQCMAAwDgYDVR0PAQH/BAQDAgeAMBYGA1Ud\nJQEB/wQMMAoGCCsGAQUFBwMCMA0GCSqGSIb3DQEBBQUAA4GBADGj8G6orcSjCznR\nUwlJ9DCoDKTJkv4W6vaRp70hbSIloBpZIfohicx19ARyxwq3R4r8kEjHS5XDIdi/\nNi1IBXCZRdpHk9z8OlyIV86uWDYoJz0vzthL/apDyIgGRxi6Pf9gGJYAHavt5TEk\n6pXqbd4LAJXuFCzBAkoorjzWn0Vx\n-----END CERTIFICATE-----\n",
       //  "c98f8f084aea475bea45c614e947dfbd10310199": "-----BEGIN CERTIFICATE-----\nMIICITCCAYqgAwIBAgIIXWYbNFqgWPAwDQYJKoZIhvcNAQEFBQAwNjE0MDIGA1UE\nAxMrZmVkZXJhdGVkLXNpZ25vbi5zeXN0ZW0uZ3NlcnZpY2VhY2NvdW50LmNvbTAe\nFw0xNDAzMTAwNjEzMzRaFw0xNDAzMTExOTEzMzRaMDYxNDAyBgNVBAMTK2ZlZGVy\nYXRlZC1zaWdub24uc3lzdGVtLmdzZXJ2aWNlYWNjb3VudC5jb20wgZ8wDQYJKoZI\nhvcNAQEBBQADgY0AMIGJAoGBAMKR9OmUxfa6wlcLy1X0NhL4FahUiGoXQFWysHwK\nrQu6Vd1THU6CtjZx52TWXI0kIyMUHdHMu5766VACq371dKGCmvKsbenC0MsIZXvr\nK89NP5nbaNs27oADKblf2fw/zFuJdQpIH6fvSLwOv96DyyaUHOk6T+8zSrb7uoTt\nXIwnAgMBAAGjODA2MAwGA1UdEwEB/wQCMAAwDgYDVR0PAQH/BAQDAgeAMBYGA1Ud\nJQEB/wQMMAoGCCsGAQUFBwMCMA0GCSqGSIb3DQEBBQUAA4GBADs5vypzOwHKddWY\nQXJEYgLkgeuMzrCP4t3MJfquDFqC0aFroRY9fkqIe6s1V8hF2ddziIWTcE0x6Z99\nvBzDlUegrIvl6L+2ibGVByAN1n5B1oH8EfgnuK1kfEhvmAeeX/mh3PISTX85gpUi\nxBgf7bbbROvWuauTPPuvkJhNerXo\n-----END CERTIFICATE-----\n"
       // }
-      var certificates = JSON.parse(certResponseText);
-
-      for (var key in certificates) {
-        if (certificates.hasOwnProperty(key)) {
-          googleCertificates.push(certificates[key]);
-        }
-      }
+      googleCertificates = JSON.parse(certResponseText);
 
       // Async callback when the certificates finish loading
       if (typeof(callback) === 'function') {
@@ -59,20 +53,12 @@ var getGoogleCertificates = function getGoogleCertificates(callback) {
 //
 // We have to use jws to verify because jwt doesn't support RS256. We have to use jwt to decode
 // because jws doesn't deserialize the payload. It only decodes it from base64.
-var verifyIdToken = function verifyIdToken(idToken, callback, secondTry) {
-  var verified = false;
-  for (var i = 0; i < googleCertificates.length; i++) {
-    try {
-      if (jws.verify(idToken, googleCertificates[i])) {
-        verified = true;
-        break;
-      }
-    } catch (e) {}
-  }
+var verifyIdToken = function verifyIdToken(kid, idToken, callback, secondTry) {
 
   // Verification will only fail if Google changes their certificate (once per day) or it really wasn't a valid
   // certificate (hacked!). If verification fails the first time try refreshing the certificates and verify again.
-  if (verified) {
+  var cert = googleCertificates[kid];
+  if (cert && jws.verify(idToken, cert)) {
     callback(true);
   }
   else if (secondTry) {
@@ -82,7 +68,7 @@ var verifyIdToken = function verifyIdToken(idToken, callback, secondTry) {
   else {
     // refresh and try again
     getGoogleCertificates(function() {
-      verifyIdToken(idToken, callback, true);
+      verifyIdToken(kid, idToken, callback, true);
     });
   }
 };
@@ -129,7 +115,9 @@ module.exports.createOpenIdConnectTokens = function createOpenIdConnectTokens(op
       // Verify the id_token against Google's certificates
       //
       // https://www.googleapis.com/oauth2/v1/certs
-      verifyIdToken(accessTokenResponseJson.id_token, function(verified) {
+      var idTokenHeader = new Buffer(accessTokenResponseJson.id_token.split('.')[0], 'base64').toString();
+      var kid = JSON.parse(idTokenHeader).kid;
+      verifyIdToken(kid, accessTokenResponseJson.id_token, function(verified) {
         if (!verified) {
           callback({ success: false, message: 'Authentication failed. The id_token signature did not verify against the certificate.' });
         }
