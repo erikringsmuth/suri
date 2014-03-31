260ca3b7a9c462a121472b83de7e7e9c2ad0f0b2
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-03-30 20:41:56 -0500

    gulp mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,38 @@
 'use strict';
 var gulp    = require('gulp'),
-    jshint  = require('gulp-jshint');
+    jshint  = require('gulp-jshint'),
+    mocha   = require('gulp-mocha');
+
+var codeFiles = [
+  'server.js',
+  'routes/**/*.js',
+  'services/**/*.js',
+  'utilities/**/*.js',
+  'app/*.js',
+  'app/components/**/*.js',
+  'app/pages/**/*.js',
+  'app/layouts/**/*.js',
+  'test/*.js'
+];
+var testFiles = 'test/*.js';
+
+gulp.task('test', function () {
+  gulp
+    .src(testFiles)
+    .pipe(mocha({ reporter: 'spec' }));
+});
 
 gulp.task('lint', function() {
   gulp
-    .src([
-      'server.js',
-      'routes/**/*.js',
-      'services/**/*.js',
-      'utilities/**/*.js',
-      'app/*.js',
-      'app/components/**/*.js',
-      'app/pages/**/*.js',
-      'app/layouts/**/*.js',
-      'test/*.js'
-    ])
+    .src(codeFiles)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('default', function(){
-  gulp.run('lint');
-
-  gulp.watch('**/*.js', function() {
-    gulp.run('lint');
+gulp.task('watch', function () {
+  gulp.watch(codeFiles, function() {
+    gulp.run('test', 'lint');
   });
 });
+
+gulp.task('default', ['test', 'lint', 'watch']);
